b2bcd4819a4b638f2689c685b32ae3cbadb1d762
Gome510 <bluehawkfire55@gmail.com>
2023-05-13 12:09:47 -0700

    added images and blank fill script

--- MMART167/Javascript1/Javascript1.js
+++ MMART167/Javascript1/Javascript1.js
@@ -5,4 +5,22 @@ Add new images with html of spaceships and aliens
 Be sure to add the date and your name as variables		
 		
 Send a link to your site on codespace or github		*/
+function madlib(){
+    let date = prompt("Enter today's date")
+    let name = prompt("Enter your name")
+    let alienName = prompt("Enter an alien name")
+    let alienYellOne = prompt("Yell something")
+    let alienYellTwo = prompt("Yell something else")
+    let adjectiveEverAfter = prompt("Enter an adverb")
 
+    document.getElementById("blankZero").innerHTML = date
+    document.getElementById("blankOne").innerHTML = name
+    document.getElementById("blankTwo").innerHTML = alienName
+    document.getElementById("blankThree").innerHTML = alienYellOne
+    document.getElementById("blankFour").innerHTML = alienYellTwo
+    document.getElementById("blankFive").innerHTML = adjectiveEverAfter
+}
+
+
+
+madlib();
